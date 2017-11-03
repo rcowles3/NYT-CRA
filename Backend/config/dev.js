c50915b1257559470e7772b9b372239909941394
rcowles3 <ryan.cowles3@gmail.com>
2017-11-03 11:55:34 -0700

    fixed articles not saving in deployment, heroku update

--- Backend/config/dev.js
+++ Backend/config/dev.js
@@ -3,5 +3,7 @@
  */
 
 module.exports = {
-  mLabURI: "mongodb://localhost:27017/nytreact"
+  // mLabURI: "mongodb://localhost:27017/nytreact"
+  mLabURI:
+    "mongodb://heroku_nxxvbp21:608lqu5e9q41ul62gk4lmnsdv3@ds125555.mlab.com:25555/heroku_nxxvbp21"
 };
