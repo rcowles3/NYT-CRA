0eb373d12010b6ccfc9efa0deb5f496ca008084b
rcowles3 <ryan.cowles3@gmail.com>
2017-11-08 18:33:58 -0700

    heroku deploy

--- Backend/config/prod.js
+++ Backend/config/prod.js
@@ -1,3 +1,13 @@
+// module.exports = {
+//   mLabURI: process.env.MONGODB_URI
+// };
+
+/**
+ * Development Keys
+ */
+
 module.exports = {
-  mLabURI: process.env.MONGODB_URI
+  // mLabURI: "mongodb://localhost:27017/nytreact"
+  mLabURI:
+    "mongodb://heroku_nxxvbp21:608lqu5e9q41ul62gk4lmnsdv3@ds125555.mlab.com:25555/heroku_nxxvbp21"
 };
