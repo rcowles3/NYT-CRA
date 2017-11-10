c56063a38f61d78de42c8ca5a39b9620564e512f
rcowles3 <ryan.cowles3@gmail.com>
2017-11-10 11:27:25 -0700

    restructured files, heroku redeploy

--- Backend/config/dev.js
+++
@@ -1,9 +0,0 @@
-/**
- * Local Development Keys
- */
-
-module.exports = {
-  // mLabURI: "mongodb://localhost:27017/nytreact"
-  mLabURI:
-    "mongodb://heroku_nxxvbp21:608lqu5e9q41ul62gk4lmnsdv3@ds125555.mlab.com:25555/heroku_nxxvbp21"
-};
