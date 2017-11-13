5b4ed12aa99430d7c68433d1e418c63e253e6477
rcowles3 <ryan.cowles3@gmail.com>
2017-11-13 01:54:57 -0700

    Migrate from create-react-app-buildpack to Node server

--- react-ui/src/index.js
+++ react-ui/src/index.js
@@ -1,20 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
-import Search from './components/Search';
-import Saved from './components/Saved';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import "./index.css";
+import App from "./App";
+// import registerServiceWorker from './registerServiceWorker';
+import Search from "./components/Search";
+import Saved from "./components/Saved";
 // import Results from './components/Results';
 
 ReactDOM.render(
-    <Router>
-        <div>
-            <Route exact={true} path='/' component={App} />            
-            <Route path='/search' component={App} />
-            <Route path='/saved' component={Saved} />
-            {/* <Route path='/results' component={Results} /> */}
-        </div>
-    </Router>, document.getElementById('root'));
-registerServiceWorker();
+  <Router>
+    <div>
+      <Route exact={true} path="/" component={App} />
+      <Route path="/search" component={App} />
+      <Route path="/saved" component={Saved} />
+      {/* <Route path='/results' component={Results} /> */}
+    </div>
+  </Router>,
+  document.getElementById("root")
+);
+// registerServiceWorker();
