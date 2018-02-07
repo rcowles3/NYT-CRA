f192c601bf60a746b44b77d66d6fb9ec60075a78
rcowles3 <ryan.cowles3@gmail.com>
2018-02-07 15:32:25 -0700

    added home buton

--- react-ui/src/components/Navbar.js
+++ react-ui/src/components/Navbar.js
@@ -3,37 +3,6 @@ import React, { Component } from "react";
 class Navbar extends Component {
   render() {
     return (
-      // <nav className="navbar navbar-default" role="navigation">
-      //   <div className="container-fluid">
-      //     <div className="navbar-header">
-      //       <button
-      //         type="button"
-      //         className="navbar-toggle"
-      //         data-toggle="collapse"
-      //         data-target=".navbar-ex1-collapse"
-      //       >
-      //         <span className="sr-only">Toggle navigation</span>
-      //         <span className="icon-bar" />
-      //         <span className="icon-bar" />
-      //         <span className="icon-bar" />
-      //       </button>
-      //       <a className="navbar-brand" href="/">
-      //         NYT-React
-      //       </a>
-      //     </div>
-      //
-      //     <div className="collapse navbar-collapse navbar-ex1-collapse">
-      //       <ul className="nav navbar-nav navbar-right">
-      //         <li>
-      //           <a href="/search">Search</a>
-      //         </li>
-      //         <li>
-      //           <a href="/saved">Saved Articles</a>
-      //         </li>
-      //       </ul>
-      //     </div>
-      //   </div>
-      // </nav>
       <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
         <button
           className="navbar-toggler"
@@ -49,6 +18,11 @@ class Navbar extends Component {
 
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav ml-auto">
+            <li className="nav-item mx-auto">
+              <a className="nav-link" href="/">
+                Search Articles
+              </a>
+            </li>
             <li className="nav-item mx-auto">
               <a className="nav-link" href="/saved">
                 Saved Articles
